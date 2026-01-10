300bce6ee9278a0d79522b07b045e43433a31011
drakeiwant <160796296+drakeiwant@users.noreply.github.com>
2026-01-10 15:47:58 +0900

    Update SplineBackground.tsx

--- components/SplineBackground.tsx
+++ components/SplineBackground.tsx
@@ -1,17 +1,17 @@
-
-import React from 'react';
+import React from "react";
 
 const SplineBackground: React.FC = () => {
   return (
-    <div className="fixed inset-0 z-0 pointer-events-none opacity-60">
-      <iframe 
-        src='https://my.spline.design/particles-24w4ZqTYUWW1kk1oPIk84poO/' 
-        frameBorder='0' 
-        width='100%' 
-        height='100%'
-        className="scale-110"
-      ></iframe>
-      <div className="absolute inset-0 bg-gradient-to-b from-transparent via-black/20 to-black pointer-events-none"></div>
+    <div className="fixed inset-0 z-0 pointer-events-none">
+      <iframe
+        src="https://my.spline.design/particles-VClcIxkq6EmpvjxzG5d3Qn4v/"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+        className="w-full h-full opacity-70"
+      />
+      {/* 어두운 그라데이션 오버레이(텍스트 가독성) */}
+      <div className="absolute inset-0 bg-gradient-to-b from-black/10 via-black/40 to-black pointer-events-none" />
     </div>
   );
 };
